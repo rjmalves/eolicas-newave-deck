1fa16d2135466e242031823788408f04639490e1
Rogerio <rogerioalves.ee@gmail.com>
2022-07-28 00:16:22 -0300

    creating installation directory

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+#! /bin/bash
+
+
+# Checks if Python 3 is installed
 command -v python3 >/dev/null 2>&1
 PYTHON3_INSTALLED=$?
 if [ $PYTHON3_INSTALLED -ne 0 ]; then
@@ -5,4 +9,26 @@ if [ $PYTHON3_INSTALLED -ne 0 ]; then
     exit $PYTHON3_INSTALLED
 fi
 
-printf "import tempfile\nprint(tempfile.gettempdir())" | python3
+# Creates installation directory in /tmp 
+TMPDIR=$(dirname $(mktemp -u))
+INSTALLDIR=${TMPDIR}/eolicas-newave-deck
+[ ! -d $INSTALLDIR ] && mkdir $INSTALLDIR
+
+# Copies necessary files
+cp -r app/ $INSTALLDIR
+cp main.py $INSTALLDIR
+cp requirements.txt $INSTALLDIR
+cp run.sh $INSTALLDIR
+
+# Creates venv is not exists
+[ ! -d $INSTALLDIR/venv ] && python3 -m venv $INSTALLDIR/venv
+
+# Activates venv and installs requirements
+CURDIR=pwd
+cd $INSTALLDIR
+source venv/bin/Activates
+pip install -r requirements.txt
+
+# Deactivates venv
+deactivate
+cd $CURDIR
