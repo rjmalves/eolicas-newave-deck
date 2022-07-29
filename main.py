782b1602df63f645af634425bb52674943db09a9
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-07-29 11:55:23 -0300

    fix uso do cfg

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from dotenv import load_dotenv
 import os
 import pathlib
 
-load_dotenv("eolicas-newave-deck.cfg", override=True)
 BASEDIR = pathlib.Path().resolve()
 os.environ["APP_INSTALLDIR"] = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(
+    pathlib.Path(os.getenv("APP_INSTALLDIR")).joinpath(
+        "eolicas-newave-deck.cfg"
+    ),
+    override=True,
+)
 os.environ["APP_BASEDIR"] = str(BASEDIR)
 load_dotenv(BASEDIR.joinpath("eolicas-newave-deck.cfg"), override=True)
 
